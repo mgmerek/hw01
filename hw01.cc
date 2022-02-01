2476939e9f302d81827d9a4768891e74f84629ed
mgmerek <97536053+mgmerek@users.noreply.github.com>
2022-01-31 19:30:09 -0500

    Add files via upload

---
+++ hw01.cc
@@ -0,0 +1,120 @@
+/**
+*File: 01-hw-invoice
+*Author: Mila Gmerek
+*Date: 1/26/2022
+*Brief: Program that processes computer monitor orders for homework #1
+*
+*/
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+
+{
+    cout << fixed << setprecision(2);
+    cout << "Enter monitor type, s or v, followed by quantity: ";
+    string monitor;
+    int quantity;
+    cin >> monitor >> quantity;
+
+    double monitor_price;
+    if (monitor == "s")
+    {
+        monitor_price = 190;
+    }
+    else if (monitor == "v")
+    {
+        monitor_price = 170;
+    }
+
+    double final_price;
+    final_price = monitor_price * quantity;
+
+    double discount_price;
+    discount_price = final_price - (final_price * 0.05);
+    double total_price;
+
+    cout << "Do you live in Ohio? (yes or no): " << endl;
+    string resident;
+    cin >> resident;
+    double tax_price;
+    const double OHIO_TAX = 0.07;
+    tax_price = (monitor_price * quantity * OHIO_TAX);
+    double discounted_final_price;
+
+    if (quantity >= 3)
+    {
+        if (resident == "yes")
+        {
+            discounted_final_price = discount_price + tax_price;
+        }
+        if (resident == "no")
+        {
+            discounted_final_price = discount_price;
+        }
+    }
+
+    if (quantity < 3)
+    {
+        if (resident == "no")
+        {
+            total_price = final_price;
+        }
+        if (resident == "yes")
+        {
+            total_price = final_price + tax_price;
+        }
+    }
+
+    cout << "-------------------------" << endl;
+    cout << "Monitor type: " << monitor << endl;
+    cout << "Quantity in order: " << quantity << endl;
+    cout << "Price per monitor: $" << monitor_price << endl;
+    cout << "Ohio Resident? " << resident << endl;
+    if (quantity >= 3)
+    {
+        cout << "Discount: $" << (monitor_price * quantity) * (.05) << endl;
+    }
+    else
+    {
+        cout << "Discount: $0.00" << endl;
+    }
+    if (resident == "yes")
+    {
+        cout << "Tax amount: $" << tax_price << endl;
+    }
+    if (resident == "no")
+    {
+        cout << "Tax amount: $0.00" << endl;
+    }
+    cout << "---------------------------" << endl;
+
+    if (quantity >= 3)
+    {
+        if (resident == "yes")
+        {
+            cout << "Subtotal: $" << discount_price + tax_price << endl;
+        }
+
+        if (resident == "no")
+        {
+            cout << "Subtotal: $ " << discounted_final_price << endl;
+        }
+    }
+    if (quantity < 3)
+    {
+        if (resident == "no")
+        {
+            cout << "Subtotal: $" << final_price << endl;
+        }
+        if (resident == "yes")
+        {
+            cout << "Subtotal: $" << total_price << endl;
+        }
+    }
+    return 0;
+}
